c50433302f27843a9bffe19afa3c2c0343f73d2f
ingagarsvaite <89849300+ingagarsvaite@users.noreply.github.com>
2024-03-27 20:54:10 +0000

    Update settings.py

--- baigiamasis_darbas/settings.py
+++ baigiamasis_darbas/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'baigiamasis_darbas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'notebooks/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
